9a3598367db3061fc313ac30069196b73df5f241
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-11-14 22:58:54 +0900

    search 페이지 생성

---
+++ pages/search/search.jsx
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router';
+import Header from '../../components/Header/Header';
+
+function Search() {
+  const router = useRouter();
+  const { search, startDate, endDate } = router.query;
+  const range = `${startDate} - ${endDate}`;
+
+  return (
+    <div>
+      <Header placeholder={`${search} | ${range}`} />
+      {range} 일자에 대한 검색어 {search}의 결과
+    </div>
+  );
+}
+
+export default Search;
